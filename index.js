e997ac4159d9ab78ecc8d272c52b2316abe71533
locbaoodo <ldo30@cccd.edu>
2024-02-18 22:56:21 -0800

    Initial setup

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-// run `node index.js` in the terminal
+const prompCreator = require('prompt-sync');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const prompt = prompCreator();
+
+// DO NOT CHANGE THE TWO LINES BELOW.
+const str = prompt("Enter a string: ");
+const n = prompt("Enter a number: ");
+console.log("str=" + str);
+console.log("n=" + n);
+
+/** 
+ * TODO: Creates a new string that repeats the given string `str` 
+ * `n` number of times. Each one should be in its own line.
+ * For example: if str is 'Hello!', and n is 3, then it should 
+ * be 'Hello!\nHello!\nHello!\n'
+ */
+let result = '';
+
+// DO NOT CHANGE THE LINES BELOW.
+console.log(result);
+
+module.exports = {
+  result,
+};
